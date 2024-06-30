c28e533026330fc6b13cfdbbbdffc986f826761a
Shashank Gupta <sg456642@gmail.com>
2024-07-01 01:11:17 +0530

    responsiveness

--- src/pages/product/Product.tsx
+++ src/pages/product/Product.tsx
@@ -1,6 +1,5 @@
 import Single from "../../components/single/Single"
-import { singleProduct } from "../../data"
-import "./product.scss"
+import { singleProduct } from "../../data";
 
 const Product = () => {
 
