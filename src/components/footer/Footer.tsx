57cc9ae5293341a6b679422f768b27b17037d324
Shashank Gupta <sg456642@gmail.com>
2024-07-01 00:36:04 +0530

    some more files added

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,12 @@
+import "./footer.scss"
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <span>Shashank</span>
+      <span>© Shashank Gupta Admin Dashboard</span>
+    </div>
+  )
+}
+
+export default Footer
